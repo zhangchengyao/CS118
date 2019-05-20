4d863824139e4ab5a04606cf55971a7b6b050a47
chengshunzhang <chengshun.zhang@outlook.com>
2019-05-20 16:21:28 -0700

    server handle connection

--- project2/server.cc
+++ project2/server.cc
@@ -19,6 +19,34 @@
 
 #define HEADER_SIZE 12
 #define BACKLOG 1 /* pending connections queue size */
+#define MAX_SEQ_NUM 25600
+
+bool handleConnection(buffer& clientPkt, int server_sockfd, sockaddr_in& their_addr, unsigned int sin_size) {
+    // choose init seq num, send SYNACK msg, acking SYN from client
+    buffer serverPkt;
+    serverPkt.hd.flags |= ((1 << 15) | (1 << 14)); // set ACKbit = 1 and SYNbit = 1
+    serverPkt.hd.seqNum = rand() % (MAX_SEQ_NUM + 1);
+    serverPkt.hd.ackNum = clientPkt.hd.seqNum + 1;
+    memset(serverPkt.data, sizeof(serverPkt.data), '\0');
+    if(sendto(server_sockfd, &serverPkt, sizeof(serverPkt), 0, (struct sockaddr*) &their_addr, sin_size) < 0) {
+        perror("send SYNACK packet");
+        return false;
+    }
+    printf("send SYNACK packet to: %s\n", inet_ntoa(their_addr.sin_addr));
+
+    // receive ACK packet from client, finish 3-way handshake
+    if(recvfrom(server_sockfd, &clientPkt, sizeof(clientPkt), 0, (struct sockaddr*) &their_addr, &sin_size) < 0) {
+        perror("receive ACK from client");
+        return false;
+    }
+    printf("receive ACK packet from: %s\n", inet_ntoa(their_addr.sin_addr));
+    if(clientPkt.hd.ackNum == serverPkt.hd.seqNum + 1) {
+        return true;
+    } else {
+        perror("ACKnum");
+        return false;
+    }
+}
 
 int main(int argc, char *argv[]){
     if(argc != 2) {
@@ -63,13 +91,24 @@ int main(int argc, char *argv[]){
     printf("waiting for a packet...\n");
 
     while(1){
+        bool connected = false;
         if((len = recvfrom(server_sockfd, &buf, sizeof(buf), 0, (struct sockaddr *) &their_addr, &sin_size)) < 0){
             perror("recvfrom error"); 
             return 1;
         }
-        printf("received packet from %s:\n", inet_ntoa(their_addr.sin_addr));
-        buf.data[len - HEADER_SIZE] = '\0';
-        printf("contents: %s\n", buf.data);
+        if((buf.hd.flags >> 14) & 1) { // SYNbit = 1
+            printf("receive connection from %s:\n", inet_ntoa(their_addr.sin_addr));
+            connected = handleConnection(buf, server_sockfd, their_addr, sin_size);
+        }
+        if(connected) {
+            printf("establish connection with %s:\n", inet_ntoa(their_addr.sin_addr));
+            // TODO receive the file
+        } else {
+            printf("received packet from: %s and dropped\n", inet_ntoa(their_addr.sin_addr));
+            buf.data[len - HEADER_SIZE] = '\0';
+            printf("contents: %s\n", buf.data);
+        }
+        
     }
     
 	close(server_sockfd);
